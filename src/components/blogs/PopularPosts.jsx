0a872bad80936bbe95e45b25f5be7ab85791669e
ikarus-atharvatamta <atharva.tamta@ikarus3d.com>
2026-01-06 19:35:59 +0530

    second commit

--- src/components/blogs/PopularPosts.jsx
+++ src/components/blogs/PopularPosts.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import bcard1 from '../../assets/bcard1.png';
+import bcard2 from '../../assets/bcard2.png';
+import bcard3 from '../../assets/bcard3.png';
 import imageplaceholder from '../../assets/Image-placeholder.png';
 export default function PopularPosts() {   
     return (  
@@ -7,7 +10,57 @@ export default function PopularPosts() {
         <h1 className='text-5xl font-bold'>Popular Post</h1>
         <button className="mt-10 text-white bg-[#DA634B]  px-10 py-4 rounded-lg font-medium">View All</button>
      </div>
-    
+    <div className="grid grid-cols-3 gap-4 px-10 py-10 max-w-7xl mx-auto">
+
+        <div className="">
+            <div>
+                <img src={bcard1} alt="" className="rounded-xl"/>
+                <p className='text-gray-500 mb-6 text-sm font-bold mt-5'>June 15, 2024</p></div>
+            <div className="font-bold text-md mb-3"><h3>Blog Title 1</h3></div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque molestias debitis aperiam neque, dolore necessitatibus!</p>
+           <a href="/blog1" className="text-[#CF462A] font-bold text-sm ">Read more...</a>
+          </div>
+ <div className="">
+            <div>
+                <img src={bcard1} alt="" className="rounded-xl"/>
+                <p className='text-gray-500 mb-6 text-sm font-bold mt-5'>June 15, 2024</p></div>
+            <div className="font-bold text-md mb-3"><h3>Blog Title 1</h3></div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque molestias debitis aperiam neque, dolore necessitatibus!</p>
+           <a href="/blog1" className="text-[#CF462A] font-bold text-sm ">Read more...</a>
+          </div>
+           <div className="">
+            <div>
+                <img src={bcard1} alt="" className="rounded-xl"/>
+                <p className='text-gray-500 mb-6 text-sm font-bold mt-5'>June 15, 2024</p></div>
+            <div className="font-bold text-md mb-3"><h3>Blog Title 1</h3></div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque molestias debitis aperiam neque, dolore necessitatibus!</p>
+           <a href="/blog1" className="text-[#CF462A] font-bold text-sm ">Read more...</a>
+          </div>
+           <div className="">
+            <div>
+                <img src={bcard1} alt="" className="rounded-xl"/>
+                <p className='text-gray-500 mb-6 text-sm font-bold mt-5'>June 15, 2024</p></div>
+            <div className="font-bold text-md mb-3"><h3>Blog Title 1</h3></div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque molestias debitis aperiam neque, dolore necessitatibus!</p>
+           <a href="/blog1" className="text-[#CF462A] font-bold text-sm ">Read more...</a>
+          </div>
+           <div className="">
+            <div>
+                <img src={bcard1} alt="" className="rounded-xl"/>
+                <p className='text-gray-500 mb-6 text-sm font-bold mt-5'>June 15, 2024</p></div>
+            <div className="font-bold text-md mb-3"><h3>Blog Title 1</h3></div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque molestias debitis aperiam neque, dolore necessitatibus!</p>
+           <a href="/blog1" className="text-[#CF462A] font-bold text-sm ">Read more...</a>
+          </div>
+           <div className="">
+            <div>
+                <img src={bcard1} alt="" className="rounded-xl"/>
+                <p className='text-gray-500 mb-6 text-sm font-bold mt-5'>June 15, 2024</p></div>
+            <div className="font-bold text-md mb-3"><h3>Blog Title 1</h3></div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque molestias debitis aperiam neque, dolore necessitatibus!</p>
+           <a href="/blog1" className="text-[#CF462A] font-bold text-sm ">Read more...</a>
+          </div>
+    </div>
         </>
       )
 
